fa092e7a5fb23f987a672479e4e9f0ae026bf20d
abdallahelabd <70246130+abdallahelabd@users.noreply.github.com>
2025-04-06 23:18:45 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBsQCz6d18CZtZQKD1NH2rNzrjiiwqLThM",
   authDomain: "abdallah-bio.firebaseapp.com",
@@ -16,6 +12,5 @@ const firebaseConfig = {
   measurementId: "G-88CM5NQ696"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
